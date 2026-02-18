4d1ad280bf7c3b3f6fba6d9a5bccd8b0ef2a9a75
Natalia Sargarovschi <natalia.sargarovschi@amdaris.com>
2026-02-18 18:48:02 +0200

    PR-2 Components refactor

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 
-export const GoalItem = ({ item }) => {
-  return <Text style={styles.goalItem}>{item}</Text>;
+export const GoalItem = ({ item, onDeteleItem }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#1c0d46" }}
+        onPress={() => onDeteleItem(item.id)}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
+        <Text style={styles.goalText}>{item.text}</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  goalText: {
+    padding: 8,
     color: "white",
   },
 });
